3ebfd79f004fc6dfb40e318e7a2d3899040c171c
Dani Matuko <matuko305@gmail.com>
2023-05-17 13:21:32 +0300

    add docs to View and Controller files

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -2,19 +2,38 @@
 
 require 'View.php';
 
+/**
+ * The Controller class handles the logic and functionality of the application's controllers.
+ */
 class Controller {
+    /**
+     * @var View The view object used for rendering views.
+     */
     protected $view;
 
-
+    /**
+     * Default action method.
+     */
     public function index() {
         // Default action
     }
 
+    /**
+     * Renders a view with the given data.
+     *
+     * @param string $view The name of the view to render.
+     * @param array $data An associative array of data to be passed to the view.
+     */
     public function render($view, $data = array()) {
         $this->view = new View();
         $this->view->render($view, $data);
     }
 
+    /**
+     * Redirects the user to the specified URL.
+     *
+     * @param string $url The URL to redirect to.
+     */
     public function redirect($url) {
         header("Location: $url");
         exit();
--- app/core/View.php
+++ app/core/View.php
@@ -1,15 +1,31 @@
 <?php
 
-
+/**
+ * The View class handles the rendering of views in the application.
+ */
 class View {
-    protected $viewDir;
+    /**
+     * @var string The directory path where the view files are located.
+     */
+    protected $viewsDir;
 
+    /**
+     * View constructor.
+     */
     public function __construct() {
-        $this->viewDir = VIEWS_DIR;
+        $this->viewsDir = VIEWS_DIR;
     }
 
+    /**
+     * Renders a view with the given data.
+     *
+     * @param string $view The name of the view file to render.
+     * @param array $data An associative array of data to be passed to the view.
+     * @return mixed The result of the included view file.
+     * @throws Exception If the view file is not found.
+     */
     public function render($view, $data = []) {
-        $viewFile = $this->viewDir . '/' . $view . '.php';
+        $viewFile = $this->viewsDir . '/' . $view . '.php';
 
         if (file_exists($viewFile)) {
             extract($data);
